ed634f6ede2d6eea4626dd200c6c04fa7eae10ea
Mehdi <amigoinpt@gmail.com>
2019-04-09 12:27:55 +0200

    Update Energy_Detection_script.m

--- EnergyDetection/Energy_Detection_script.m
+++ EnergyDetection/Energy_Detection_script.m
@@ -3,9 +3,7 @@
 clear                                  % Clear all data stored in variables
 Am = 1;                                % 1V Amplitude     
 SR = 48000;                            % 48 KHz Smapling Rate
-f0 = 13e+3;                            % 20 KHz
-f1 = 10e+3;                            % 10 KHz
-f2 = 4e+3;                             % 10 KHz
+f0 = 20e+3;                            % 20 KHz
 duration = (127/SR);                   % ~ 2.6 ms 
 t = 0:1/SR:duration;                   % Time Vector
 %%
@@ -14,11 +12,20 @@ mySNR = -30:1:30;           % SNR buffer
 threshold = 150;
 snrValues(sinusPulse,mySNR,threshold)
 %%
-function snrValues(yourSignal,snr,threshold) 
+function detectProbability(yourSignal,snr,threshold) 
+N = length(yourSignal);
     for i = 1:length(snr)
-        yourSignal = awgn(yourSignal,snr(i));
-        find_PD_PFa_ED(yourSignal,threshold);        
-        load Pd_Pfa_ED.mat Pd_ED      
+        Pd_buff(100) = 0;
+        for j = 1:100
+            a(N) = 100;
+            yourSignal = awgn(yourSignal,snr(i));
+            a(1) = abs(yourSignal(1)).^2;    
+            for k = 2:N
+                a(k) = a(k-1) + abs(yourSignal(k)).^2;
+            end
+            Pd_buff(j) = sum(a > threshold)/N;
+        end
+         
         plot(snr(i),Pd_ED,'b+');
         hold on
         title('Energy Detection')
@@ -30,25 +37,3 @@ end
 %% 
 % 
 
-function find_PD_PFa_ED(yourSignal,threshold)
-    Pd_buff(100)=0;
-    Pfa_buff(100)=0;
-    for j = 1:100
-        highValue = 0;
-        lowValue = 0;
-        a = 0;
-        for i = 1:length(yourSignal)
-            a = a + abs(yourSignal(i)).^2;
-            if a > threshold
-                highValue = highValue + 1;
-            else
-                lowValue = lowValue + 1;
-            end
-        end
-        Pd_buff(j) = highValue/length(yourSignal);
-        Pfa_buff(j) = lowValue/length(yourSignal);
-    end
-    Pd_ED = sum(Pd_buff)/100;
-    Pfa_ED = sum(Pfa_buff)/100;
-    save Pd_Pfa_ED.mat a Pd_ED Pfa_ED
-end
